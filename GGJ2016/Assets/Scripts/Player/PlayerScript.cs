2827aabc1e19f3f190ff9c7d563866ace9cc42bc
Charles Pearson <contact@hazegames.com>
2016-01-29 23:55:19 +0100

    Added prefabs

--- GGJ2016/Assets/Scripts/Player/PlayerScript.cs
+++ GGJ2016/Assets/Scripts/Player/PlayerScript.cs
@@ -5,16 +5,23 @@ public class PlayerScript : MonoBehaviour
 {
     public Vector3 m_startPosition;
 	public float m_movementSpeed = 5f;
-	
+    private CharacterController m_characterController;
 
 	// Use this for initialization
 	void Start ()
     {
-	
+        m_characterController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        ApplyGravity();
+        
 	}
+
+    private void ApplyGravity()
+    {
+        //m_characterController.Move(new vecto)
+    }
 }
