f295cb3bd1dc1b8a7b37fb01cea1889f145325b9
Charles Pearson <contact@hazegames.com>
2016-01-29 23:33:11 +0100

    Start player script

--- GGJ2016/Assets/Scripts/Player/PlayerScript.cs
+++ GGJ2016/Assets/Scripts/Player/PlayerScript.cs
@@ -1,29 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScript : MonoBehaviour {
-	float xpos;
-	float ypos;
-	const float speed = 0.2f;
+public class PlayerScript : MonoBehaviour
+{
+    public Vector3 m_startPosition;
+	public float m_movementSpeed = 5f;
+	
 
 	// Use this for initialization
-	void Start () {
-		// Set up initial coordinates
-		xpos = transform.position.x;
-		ypos = transform.position.y;	
+	void Start ()
+    {
+	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		// Get the input from mouse keys
-		float dx = Input.GetAxis("Horizontal");
-		float dy = Input.GetAxis("Vertical");
-		Debug.Log("dx: " + dx + ", dy: " + dy);
-
-		xpos += speed * dx;
-		ypos += speed * dy;
-
-		transform.SetXPosition(xpos);
-		transform.SetYPosition(ypos);
+	void Update ()
+    {
 	}
 }
