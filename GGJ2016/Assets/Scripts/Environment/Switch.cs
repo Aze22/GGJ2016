becad08efc37fd3818f864b8ad873c8b767a22b8
Bilen Ahmet <bilen.ahmet@metaswitch.com>
2016-01-30 01:19:34 +0000

    Moving camera with character and not relying on Switch having a SpriteRenderer

--- GGJ2016/Assets/Scripts/Environment/Switch.cs
+++ GGJ2016/Assets/Scripts/Environment/Switch.cs
@@ -7,10 +7,12 @@ public class Switch : MonoBehaviour
 	// This function should update the switch asset to an appropriate state (e.g. change graphic)
 	public void SetState(bool state) {
 		Debug.Log("Setting switch state to " + state);
+
+		// Modify this code to toggle the different states, based on the final switch assets
 		if (state) {
-			gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+			//gameObject.GetComponent<SpriteRenderer>().color = Color.red;
 		} else {
-			gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+			//gameObject.GetComponent<SpriteRenderer>().color = Color.green;
 		}
 	}
 }
--- GGJ2016/Assets/Scripts/Player/PlayerScript.cs
+++ GGJ2016/Assets/Scripts/Player/PlayerScript.cs
@@ -3,17 +3,32 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour
 {
+	// Public variables
     public Vector3 m_startPosition;
 	public float m_movementSpeed = 5f;
     public float m_gravityMultiplier = 1f;
+
+    // Private variables
     private CharacterController m_characterController;
+    private Camera m_camera;
     private Vector3 m_finalMovement;
+    private Vector3 m_cameraOffset;
+
+    // Constants
+    private const float CAMERA_BOX_X = 2f;
+	private const float CAMERA_BOX_Y = 2f;
+	private const float CAMERA_BOX_Z = 2f;
 
 	// Use this for initialization
 	void Start ()
     {
+    	// Set up the character controller and movement
         m_characterController = GetComponent<CharacterController>();
         m_finalMovement = Vector3.zero;
+
+        // Set up the camera and offset
+        m_camera = FindObjectOfType(typeof(Camera)) as Camera;
+        m_cameraOffset = m_camera.transform.position - transform.position;
 	}
 	
 	// Update is called once per frame
@@ -23,13 +38,16 @@ public class PlayerScript : MonoBehaviour
         ProcessMovement();
         ProcessGravity();
         ApplyFinalMovement();
+        UpdateCamera();
 	}
 
+	// Initializes movement
     public void EarlySetup()
     {
         m_finalMovement = Vector3.zero;
     }
 
+    // Function to check movement inputs
     private void ProcessMovement()
     {
         float h = Input.GetAxis("Horizontal");
@@ -39,13 +57,41 @@ public class PlayerScript : MonoBehaviour
         m_finalMovement *= m_movementSpeed;
     }
 
+    // Function to handle gravity
     private void ProcessGravity()
     {
         m_finalMovement += new Vector3(0, Physics.gravity.y * m_gravityMultiplier, 0);  
     }
 
+    // Function to move the character
     public void ApplyFinalMovement()
     {
         m_characterController.Move(m_finalMovement * Time.deltaTime);
+    }
+
+    // Function to update the camera position, based on character position
+    private void UpdateCamera() {
+    	Vector3 centreOffset = (m_camera.transform.position - transform.position) - m_cameraOffset;
+    	Vector3 cameraDiff = Vector3.zero;
+
+		if (centreOffset.x > CAMERA_BOX_X) {
+			cameraDiff.x = centreOffset.x - CAMERA_BOX_X;
+		} else if (centreOffset.x < -CAMERA_BOX_X) {
+			cameraDiff.x = centreOffset.x + CAMERA_BOX_X;
+		}
+
+		if (centreOffset.y > CAMERA_BOX_Y) {
+			cameraDiff.y = centreOffset.y - CAMERA_BOX_Y;
+		} else if (centreOffset.y < -CAMERA_BOX_Y) {
+			cameraDiff.y = centreOffset.y + CAMERA_BOX_Y;
+		}
+
+		if (centreOffset.z > CAMERA_BOX_Z) {
+			cameraDiff.z = centreOffset.z - CAMERA_BOX_Z;
+		} else if (centreOffset.z < -CAMERA_BOX_Z) {
+			cameraDiff.z = centreOffset.z + CAMERA_BOX_Z;
+		}
+
+		m_camera.transform.position -= cameraDiff;
     }
 }
