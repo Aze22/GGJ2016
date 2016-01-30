420d3acb12640b15ada576357e83c40b15b80823
dimixitas <dimixitas@gmail.com>
2016-01-30 06:46:47 +0000

    Added clock and sprites

---
+++ GGJ2016/Assets/Scripts/UI/Clock_Anim.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+
+public class Clock_Anim : MonoBehaviour
+{
+    public Transform cog;
+
+    private const float
+       RotationSpeed = 360f / 60f;
+
+    public bool analog;
+
+
+    void Update()
+    {
+        if (analog)
+        {
+            TimeSpan timespan = DateTime.Now.TimeOfDay;
+            cog.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalSeconds * -RotationSpeed);
+        }
+        else
+        {
+            DateTime time = DateTime.Now;
+            cog.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -RotationSpeed);
+ 
+
+        }
+
+    }
+}
+
