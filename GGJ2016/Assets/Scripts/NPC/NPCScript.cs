82b376ac185cc5dcdbd24bb14ec6eb100ef6e614
Kavita <kavita@whatsmysize.com>
2016-01-30 17:44:54 +0000

    not working NPC Script...

--- GGJ2016/Assets/Scripts/NPC/NPCScript.cs
+++ GGJ2016/Assets/Scripts/NPC/NPCScript.cs
@@ -14,25 +14,31 @@ public class NPCScript : MonoBehaviour
     	Reciprocal,
     	Circular,
     	Follow,
-    	Copy,
-    	Mirror,
-    	Path
+  //  	Copy,
+  //  	Mirror,
+  //  	Path
     };
+	public NpcBehaviour behaviour; 
 
     // Private variables
     private CharacterController m_npcController;
     private Vector3 m_finalMovement;
     private Vector3 m_cameraOffset;
     float value = 0.000f;
+	Transform myTransform;
+	float h = 0, v = 0;
+	public int z = 5;
+
 
     // Constants
 
     // Use this for initialization
 	void Start ()
-    {
+	{
     	// Set up the character controller and movement
-        m_npcController = GetComponent<CharacterController>();
+        m_npcController = GetComponentInParent<CharacterController>();
         m_finalMovement = Vector3.zero;
+		behaviour = NpcBehaviour.Linear       ;
 	}
 
 	// Update is called once per frame
@@ -50,17 +56,74 @@ public class NPCScript : MonoBehaviour
         m_finalMovement = Vector3.zero;
     }
 
+	void Awake()
+	{
+		myTransform = transform;
+	}
+
     // Function to check movement inputs
     private void ProcessMovement()
-    {
-        float h, v;
-        v = Mathf.Cos(value);
-        value += 0.05f;
-        h = Mathf.Sin(value);
-     
-        m_finalMovement += new Vector3(v, 0, -h);
-        m_finalMovement *= m_movementSpeed;
-    }
+	{
+		//Static, Linear, Reciprocal, Circular, Follow, 
+		//To Do Copy, Mirror, Path
+		if (behaviour == NpcBehaviour.Static) {
+			z = 1;
+		}
+		if (behaviour == NpcBehaviour.Linear) {
+			z = 2;
+		}
+		if (behaviour == NpcBehaviour.Reciprocal) {
+			z = 3;
+		}
+		if (behaviour == NpcBehaviour.Circular) {
+			z = 4;
+		}
+		if (behaviour == NpcBehaviour.Follow) {
+			z = 5;
+		}
+
+		switch (z) {
+		case 1:
+			// set everything to stop but this doesn't work 
+			//myTransform.position = new Vector3(1.0f, 1.0f, 1.0f);
+			m_finalMovement += new Vector3 (0, 0, 0);
+			break; 
+		case 2:
+			//going straight on the horizontal
+			h += 0.1f;
+			m_finalMovement += new Vector3 (0, 0, h);
+			break;
+		case 3:
+			//going straight on the vertical
+			h += 0.1f;
+			m_finalMovement += new Vector3 (0, 0, -h);
+			break;
+		case 4:
+			value += 5;
+			v = Mathf.Cos (value);
+			h = Mathf.Sin (value);
+			m_finalMovement += new Vector3 (v, 0, h);
+			break;
+		case 5:
+			Transform target;
+			target = GameObject.FindWithTag ("Player").transform;
+
+			myTransform.rotation = Quaternion.Slerp (myTransform.rotation,
+				Quaternion.LookRotation (target.position - myTransform.position), 3f * Time.deltaTime);
+			myTransform.position += myTransform.forward * m_movementSpeed * Time.deltaTime;
+
+
+
+
+			break;
+		default:
+			// hello
+			break;
+		}
+
+	}		
+
+    
 
     // Function to handle gravity
     private void ProcessGravity()
