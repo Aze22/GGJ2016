ef92f8433e0191fecc843137463efdf9ee4a389d
Charles Pearson <contact@hazegames.com>
2016-01-30 10:28:10 +0100

    Started transparency

--- GGJ2016/Assets/Scripts/Player/CameraScript.cs
+++ GGJ2016/Assets/Scripts/Player/CameraScript.cs
@@ -3,5 +3,45 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour
 {
-   // public 
+    public Material transparentMat;
+    public Material normalMat;
+
+    public Transform player;
+    public LayerMask layer;
+
+    public Transform raycastCheck;
+
+    private Renderer currentHiddenObject = null;
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(raycastCheck.position, player.position + new Vector3(0,-2f,0), out hit, 2000, layer))
+        {
+            Debug.Log(hit.collider.name);
+            if (hit.collider != null && hit.collider.name.Contains("Wall"))
+            {
+                
+
+                if (currentHiddenObject != null)
+                    currentHiddenObject.sharedMaterial = normalMat;
+
+                currentHiddenObject = hit.collider.GetComponent<Renderer>();
+
+                if (currentHiddenObject != null)
+                    currentHiddenObject.sharedMaterial = transparentMat;
+            }
+            else
+            {
+                if (currentHiddenObject != null)
+                    currentHiddenObject.sharedMaterial = normalMat;
+            }
+        }
+        else
+        {
+            if (currentHiddenObject != null)
+                currentHiddenObject.sharedMaterial = normalMat;
+        }
+    }
 }
