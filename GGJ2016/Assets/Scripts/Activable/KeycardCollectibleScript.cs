40a3b3a1351a4c38e01debc5258da5af7f4e5175
Charles Pearson <contact@hazegames.com>
2016-01-30 03:20:30 +0100

    Added Keycard different types / colors

--- GGJ2016/Assets/Scripts/Activable/KeycardCollectibleScript.cs
+++ GGJ2016/Assets/Scripts/Activable/KeycardCollectibleScript.cs
@@ -5,7 +5,47 @@ public class KeycardCollectibleScript : CollectibleScript
 {
     public GameStateManager.KeyCards m_keyType;
 
-	public override void Pickup()
+    [HideInInspector]
+    public Material redMat;
+    [HideInInspector]
+    public Material greenMat;
+    [HideInInspector]
+    public Material cyanMat;
+    [HideInInspector]
+    public Material blueMat;
+    [HideInInspector]
+    public Material yellowMat;
+    [HideInInspector]
+    public Material magentaMat;
+
+    public Renderer m_meshRenderer;
+
+    public override void Start()
+    {
+        switch(m_keyType)
+        {
+            case GameStateManager.KeyCards.Red:
+                m_meshRenderer.sharedMaterial = redMat;
+                break;
+            case GameStateManager.KeyCards.Green:
+                m_meshRenderer.sharedMaterial = greenMat;
+                break;
+            case GameStateManager.KeyCards.Cyan:
+                m_meshRenderer.sharedMaterial = cyanMat;
+                break;
+            case GameStateManager.KeyCards.Blue:
+                m_meshRenderer.sharedMaterial = blueMat;
+                break;
+            case GameStateManager.KeyCards.Yellow:
+                m_meshRenderer.sharedMaterial = yellowMat;
+                break;
+            case GameStateManager.KeyCards.Magenta:
+                m_meshRenderer.sharedMaterial = magentaMat;
+                break;
+        }
+    }
+
+    public override void Pickup()
     {
         GameStateManager.Instance.CollectKeyCard(m_keyType);
         base.Pickup();
